4358cd0bc9bc5f5846392440c5bf7fc2eb234512
srtalaie <srt4721@yahoo.com>
2023-06-05 15:13:05 -0700

    sepia done

--- problem set 4/helper.c
+++ problem set 4/helper.c
@@ -24,6 +24,33 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      float sepiaRed = (.393 * image[i][j].rgbtRed) + (.769 * image[i][j].rgbtGreen) + (.189 * image[i][j].rgbtBlue);
+      float sepiaGreen = (.349 * image[i][j].rgbtRed) + (.686 * image[i][j].rgbtGreen) + (.168 * image[i][j].rgbtBlue);
+      float sepiaBlue = (.272 * image[i][j].rgbtRed) + (.534 * image[i][j].rgbtGreen) + (.131 * image[i][j].rgbtBlue);
+
+      if (sepiaRed > 255)
+      {
+        sepiaRed = 255;
+      }
+      if (sepiaBlue > 255)
+      {
+        sepiaBlue = 255;
+      }
+      if (sepiaGreen > 255)
+      {
+        sepiaGreen = 255;
+      }
+
+      image[i][j].rgbtRed = round(sepiaRed);
+      image[i][j].rgbtBlue = round(sepiaBlue);
+      image[i][j].rgbtGreen = round(sepiaGreen);
+    }
+  }
+
   return;
 }
 
