1a2cb6807ada98c824aa0db68bd8db13cb8f4ede
srtalaie <srt4721@yahoo.com>
2023-06-07 14:41:02 -0700

    blur

--- problem set 4/helper.c
+++ problem set 4/helper.c
@@ -74,6 +74,51 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+  RGBTRIPLE copy[height][width];
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      copy[i][j] = image[i][j];
+    }
+  }
+
+  for (int i = 0; i < height; i++)
+  {
+    for (int j = 0; j < width; j++)
+    {
+      int red_total = 0;
+      int green_total = 0;
+      int blue_total = 0;
+      float total_pixels = 0;
+
+      for (int neighbor_column = -1; neighbor_column < 2; neighbor_column++)
+      {
+        for (int neighbor_row = -1; neighbor_row < 2; neighbor_row++)
+        {
+          if (i + neighbor_column < 0 || i + neighbor_column > (height - 1))
+          {
+            continue;
+          }
+
+          if (j + neighbor_row < 0 || j + neighbor_row > (width - 1))
+          {
+            continue;
+          }
+
+          red_total += copy[i + neighbor_column][j + neighbor_row].rgbtRed;
+          green_total += copy[i + neighbor_column][j + neighbor_row].rgbtGreen;
+          blue_total += copy[i + neighbor_column][j + neighbor_row].rgbtBlue;
+          total_pixels++;
+        }
+      }
+
+      image[i][j].rgbtRed = round(red_total / total_pixels);
+      image[i][j].rgbtGreen = round(green_total / total_pixels);
+      image[i][j].rgbtBlue = round(blue_total / total_pixels);
+    }
+  }
+
   return;
 }
 
