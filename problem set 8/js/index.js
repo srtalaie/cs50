bbf7f10f66c6c20232f2a8f474078f61f48048a9
srtalaie <srt4721@yahoo.com>
2023-08-09 14:38:19 -0700

    pset 8 done

--- problem set 8/js/index.js
+++ problem set 8/js/index.js
@@ -3,6 +3,10 @@ let year = date.getFullYear()
 
 document.addEventListener("DOMContentLoaded", function () {
 	let copyFooter = document.getElementById("copy-date")
-	console.log(copyFooter)
 	copyFooter.innerHTML = `&copy${year}`
 })
+
+function submitForm() {
+	let commentSectionText = document.getElementById("comment-section").value
+	alert(`Message: ${commentSectionText}`)
+}
