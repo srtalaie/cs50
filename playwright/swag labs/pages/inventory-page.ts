59464311736daa134192d344a14d17aa3bb28b92
srtalaie <srt4721@yahoo.com>
2025-06-19 21:08:10 -0700

    product page

--- playwright/swag labs/pages/inventory-page.ts
+++ playwright/swag labs/pages/inventory-page.ts
@@ -1,4 +1,4 @@
-import { type Locator, type Page } from '@playwright/test';
+import { type Locator, type Page } from '@playwright/test'
 
 export class InventoryPage {
   readonly page: Page
@@ -8,4 +8,13 @@ export class InventoryPage {
   readonly productSort: Locator
   readonly shoppingCart: Locator
   readonly burgerMenu: Locator
+
+  constructor(page: Page) {
+    this.page = page
+    this.appLogo = page.locator('.header_label').locator('.app_logo', { hasText: 'Swag Labs' })
+    this.pageTitle = page.locator('.header_secondary_container').locator('.title', { hasText: 'Products' })
+    this.productSort = page.locator('.right_component').locator('.product_sort_container')
+    this.shoppingCart = page.locator('.shopping_cart_container').locator('.shopping_cart_link')
+    this.burgerMenu = page.locator('#menu_button_container').locator('.bm-burger-button').getByRole('button')
+  }
 }
--- playwright/swag labs/tests/auth.setup.ts
+++ playwright/swag labs/tests/auth.setup.ts
@@ -1,7 +1,7 @@
-import { test as setup } from '@playwright/test';
-import path from 'path';
+import { test as setup } from '@playwright/test'
+import path from 'path'
 
-const authFile = path.join(__dirname, '../playwright/.auth/user.json');
+const authFile = path.join(__dirname, '../playwright/.auth/user.json')
 
 const creds = {
   user_name: process.env.USER_NAME as string,
@@ -9,15 +9,15 @@ const creds = {
 }
 
 setup('authenticate', async ({ page, baseURL }) => {
-  await page.goto('/');
+  await page.goto('/')
 
-  await page.getByPlaceholder('Username').fill(creds.user_name);
-  await page.getByPlaceholder('Password').fill(creds.password);
+  await page.getByPlaceholder('Username').fill(creds.user_name)
+  await page.getByPlaceholder('Password').fill(creds.password)
 
   await page.locator('input[type="submit"]').click()
 
   await page.waitForURL(`${baseURL}/inventory.html`)
 
-  await page.context().storageState({ path: authFile });
+  await page.context().storageState({ path: authFile })
 
 })
